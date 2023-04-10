4debac6fd2405230d942049a51d04416aede2ba1
akmarwah03 <amarwah4@my.bcit.ca>
2023-04-09 22:11:09 -0700

    Finished report

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 import Report from "./Report";
 
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: #f44336;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #d32f2f;
+  }
+`;
 
 function Dashboard({ accessToken, setAccessToken, refreshToken }) {
+  const navigate = useNavigate();
+  const logoutHandler = () => {
+    navigate("/");
+  };
   return (
-    <div>
+    <div style={{ width: "90%", margin: "0 auto" }}>
       <h1>Dashboard</h1>
       <Report
         accessToken={accessToken}
         setAccessToken={setAccessToken}
         refreshToken={refreshToken}
       />
+      <Button onClick={logoutHandler}>Logout</Button>
     </div>
   );
 }
--- src/Components/Report.jsx
+++ src/Components/Report.jsx
@@ -1,27 +1,74 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
-import { Bar } from "react-chartjs-2";
+import styled from "styled-components";
+
+const Table = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  th,
+  td {
+    text-align: left;
+    padding: 8px;
+    border-bottom: 1px solid #ddd;
+  }
+  th {
+    background-color: #f2f2f2;
+  }
+`;
 
 function Report({ id, accessToken, setAccessToken, refreshToken }) {
+  const [uniqueAPI, setUniqueAPI] = useState([]);
+  const [topAPI, setTopAPI] = useState([]);
+  const [topEndpoints, setTopEndpoints] = useState([]);
+  const [errors, setErrors] = useState([]);
+  const [recentErrors, setRecentErrors] = useState([]);
   useEffect(() => {
     async function fetchData() {
-      // Fetch data from API
-      const usersByTime = {};
-      const response = await axiosJWT.get("http://localhost:3000/api/report", {
+      let response = await axiosJWT.get(
+        "http://localhost:3000/api/uniqueAPIUsers",
+        {
+          headers: {
+            Authorization: accessToken,
+          },
+        }
+      );
+      let data = await response.data;
+      setUniqueAPI(data);
+      response = await axiosJWT.get("http://localhost:3000/api/topAPIUsers", {
         headers: {
           Authorization: accessToken,
         },
       });
-      const data = await response.data;
-      data.forEach((entry) => {
-        const time = entry.createdAt.slice(0, 10); // Extract date portion of createdAt timestamp
-        if (!usersByTime[time]) {
-          usersByTime[time] = new Set();
+      data = await response.data;
+      setTopAPI(data);
+      response = await axiosJWT.get(
+        "http://localhost:3000/api/topEndpointUsers",
+        {
+          headers: {
+            Authorization: accessToken,
+          },
         }
-        usersByTime[time].add(entry.user);
+      );
+      data = await response.data;
+      setTopEndpoints(data);
+      response = await axiosJWT.get(
+        "http://localhost:3000/api/errorByEndPoint",
+        {
+          headers: {
+            Authorization: accessToken,
+          },
+        }
+      );
+      data = await response.data;
+      setErrors(data);
+      response = await axiosJWT.get("http://localhost:3000/api/recentErrors", {
+        headers: {
+          Authorization: accessToken,
+        },
       });
-      console.log(Object.values(usersByTime).map((users) => users.size));
+      data = await response.data;
+      setRecentErrors(data);
     }
 
     fetchData();
@@ -65,6 +112,104 @@ function Report({ id, accessToken, setAccessToken, refreshToken }) {
   return (
     <div>
       <h2>Unique API Users Over the Last Week</h2>
+      <Table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Unique API Users</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(uniqueAPI).map(([date, userCount]) => (
+            <tr key={date}>
+              <td>{date}</td>
+              <td>{userCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <h2>Top API Users Over the Last Week</h2>
+      <Table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Top API Users</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(topAPI).map(([date, users]) => (
+            <tr key={date}>
+              <td>{date}</td>
+              <td>
+                {users.map((user, index) => (
+                  <div key={index}>{user}</div>
+                ))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <h2>Top API Users Over each Endpoint</h2>
+      <Table>
+        <thead>
+          <tr>
+            <th>API Endpoint</th>
+            <th>HTTP Method</th>
+            <th>Users</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(topEndpoints).map(([endpoint, methods]) =>
+            Object.entries(methods).map(([method, users]) => (
+              <tr key={`${endpoint}-${method}`}>
+                <td>{endpoint}</td>
+                <td>{method}</td>
+                <td>{users.join(", ")}</td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </Table>
+      <h2>4xx Errors By Endpoint</h2>
+      <Table>
+        <thead>
+          <tr>
+            <th>API Endpoint</th>
+            <th>Error Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(errors).map(([endpoint, errors]) => (
+            <tr key={endpoint}>
+              <td>{endpoint}</td>
+              <td>{errors.join("\n ")}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <h2>Recent 4xx/5xx Errors</h2>
+      <Table>
+        <thead>
+          <tr>
+            <th>Endpoint</th>
+            <th>Type</th>
+            <th>Code</th>
+            <th>Message</th>
+            <th>Created At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {recentErrors.map((entry) => (
+            <tr key={entry._id}>
+              <td>{entry.endpoint}</td>
+              <td>{entry.type}</td>
+              <td>{entry.code}</td>
+              <td>{entry.message}</td>
+              <td>{entry.createdAt}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
